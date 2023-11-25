e99d6ea837f87000b9f524176a3ea197e2c7f3a4
oyeoncho <90175193+oyeoncho@users.noreply.github.com>
2023-11-25 15:05:10 +0900

    Update log2fc.R

--- log2fc.R
+++ log2fc.R
@@ -21,6 +21,12 @@ for (i in 1:nrow(miRa1)) {
 miRa1_num <- cbind(miRa1, num) %>% 
   as_tibble() 
 
+sum(miRa1_num$num)/(nrow(miRa1)*84)
+z <- miRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+
+miRa1 %>% ggplot(aes(num))+geom_bar()
+
 mirna <- miRa1_num %>% filter(num<42) %>% select(-num)
 
 
@@ -40,10 +46,11 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=10000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  mirna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  mirna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue, normEXP$FDR)
   colnames(mirna_fc)[1] <- "Mature_ID"
   colnames(mirna_fc)[2] <- str_c(str_sub(colnames(mirna)[[i]], 1, 8), "_FC")
   colnames(mirna_fc)[3] <- str_c(str_sub(colnames(mirna)[[i]], 1, 8), "_pvalue")
+  colnames(mirna_fc)[4] <- str_c(str_sub(colnames(mirna)[[i]], 1, 8), "_FDR")
   mirna <- merge(mirna, mirna_fc, by="Mature_ID")
 }
 
@@ -66,6 +73,11 @@ for (i in 1:nrow(piRa1)) {
 piRa1_num <- cbind(piRa1, num) %>% 
   as_tibble() 
 
+sum(piRa1_num$num)/(nrow(piRa1)*84)
+z <- piRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+piRa1 %>% ggplot(aes(num))+geom_bar()
+
 pirna <- piRa1_num %>% filter(num<42) %>% select(-num)
 
 for (i in 2:43) {
@@ -84,10 +96,11 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=10000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  pirna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  pirna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue , normEXP$FDR)
   colnames(pirna_fc)[1] <- "SeqID"
   colnames(pirna_fc)[2] <- str_c(str_sub(colnames(pirna)[[i]], 1, 8), "_FC")
   colnames(pirna_fc)[3] <- str_c(str_sub(colnames(pirna)[[i]], 1, 8), "_pvalue")
+  colnames(pirna_fc)[4] <- str_c(str_sub(colnames(pirna)[[i]], 1, 8), "_FDR")
   pirna <- merge(pirna, pirna_fc, by="SeqID")
 }
 
@@ -109,6 +122,11 @@ for (i in 1:nrow(snoRa1)) {
 snoRa1_num <- cbind(snoRa1, num) %>% 
   as_tibble() 
 
+sum(snoRa1_num$num)/(nrow(snoRa1)*84)
+z <- snoRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+snoRa1 %>% ggplot(aes(num))+geom_bar()
+
 snorna <- snoRa1_num %>% filter(num<42) %>% select(-num)
 
 for (i in 2:43) {
@@ -127,10 +145,11 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=10000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  snorna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  snorna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue , normEXP$FDR)
   colnames(snorna_fc)[1] <- "SeqID"
   colnames(snorna_fc)[2] <- str_c(str_sub(colnames(snorna)[[i]], 1, 8), "_FC")
   colnames(snorna_fc)[3] <- str_c(str_sub(colnames(snorna)[[i]], 1, 8), "_pvalue")
+  colnames(snorna_fc)[4] <- str_c(str_sub(colnames(pirna)[[i]], 1, 8), "_FDR")
   snorna <- merge(snorna, snorna_fc, by="SeqID")
 }
 
@@ -152,6 +171,11 @@ for (i in 1:nrow(snRa1)) {
 snRa1_num <- cbind(snRa1, num) %>% 
   as_tibble() 
 
+sum(snRa1_num$num)/(nrow(snRa1)*84)
+z <- snRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+snRa1 %>% ggplot(aes(num))+geom_bar()
+
 snRna <- snRa1_num %>% filter(num<42) %>% select(-num)
 
 for (i in 2:43) {
@@ -170,10 +194,11 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=10000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  snRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  snRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue, normEXP$FDR)
   colnames(snRna_fc)[1] <- "SeqID"
   colnames(snRna_fc)[2] <- str_c(str_sub(colnames(snRna)[[i]], 1, 8), "_FC")
   colnames(snRna_fc)[3] <- str_c(str_sub(colnames(snRna)[[i]], 1, 8), "_pvalue")
+  colnames(snRna_fc)[4] <- str_c(str_sub(colnames(snRna)[[i]], 1, 8), "_FDR")
   snRna <- merge(snRna, snRna_fc, by="SeqID")
 }
 
@@ -196,6 +221,11 @@ for (i in 1:nrow(tRa1)) {
 tRa1_num <- cbind(tRa1, num) %>% 
   as_tibble() 
 
+sum(tRa1_num$num)/(nrow(tRa1)*84)
+z <- tRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+tRa1 %>% ggplot(aes(num))+geom_bar()
+
 tRna <- tRa1_num %>% filter(num<42) %>% select(-num)
 
 for (i in 2:43) {
@@ -214,10 +244,11 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=10000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  tRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  tRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue, normEXP$FDR)
   colnames(tRna_fc)[1] <- "SeqID"
   colnames(tRna_fc)[2] <- str_c(str_sub(colnames(tRna)[[i]], 1, 8), "_FC")
   colnames(tRna_fc)[3] <- str_c(str_sub(colnames(tRna)[[i]], 1, 8), "_pvalue")
+  colnames(tRna_fc)[4] <- str_c(str_sub(colnames(tRna)[[i]], 1, 8), "_FDR")
   tRna <- merge(tRna, tRna_fc, by="SeqID")
 }
 
@@ -240,6 +271,11 @@ for (i in 1:nrow(yRa1)) {
 yRa1_num <- cbind(yRa1, num) %>% 
   as_tibble() 
 
+sum(yRa1_num$num)/(nrow(yRa1)*84)
+z <- yRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+yRa1 %>% ggplot(aes(num))+geom_bar()
+
 yRna <- yRa1_num %>% filter(num<42) %>% select(-num)
 
 for (i in 2:43) {
@@ -258,16 +294,16 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=10000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  yRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  yRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue, normEXP$FDR)
   colnames(yRna_fc)[1] <- "SeqID"
   colnames(yRna_fc)[2] <- str_c(str_sub(colnames(yRna)[[i]], 1, 8), "_FC")
   colnames(yRna_fc)[3] <- str_c(str_sub(colnames(yRna)[[i]], 1, 8), "_pvalue")
+  colnames(yRna_fc)[4] <- str_c(str_sub(colnames(yRna)[[i]], 1, 8), "_FDR")
   yRna <- merge(yRna, yRna_fc, by="SeqID")
 }
 
 # select 700x 42
 yR <-  yRna %>%  select(SeqID, contains("_FC"), contains("_pvalue")) %>% as_tibble() 
-
 
 
 ########## mR
@@ -285,6 +321,11 @@ for (i in 1:nrow(mRa1)) {
 
 mRa1_num <- cbind(mRa1, num) %>% 
   as_tibble() 
+
+sum(mRa1_num$num)/(nrow(mRa1)*84)
+z <- mRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+mRa1 %>% ggplot(aes(num))+geom_bar()
 
 mrna <- mRa1_num %>% filter(num<42) %>% select(-num)
 
@@ -305,10 +346,11 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=30000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  mrna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  mrna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue, normEXP$FDR)
   colnames(mrna_fc)[1] <- "Gene_Symbol"
   colnames(mrna_fc)[2] <- str_c(str_sub(colnames(mrna)[[i]], 1, 8), "_FC")
   colnames(mrna_fc)[3] <- str_c(str_sub(colnames(mrna)[[i]], 1, 8), "_pvalue")
+  colnames(mrna_fc)[4] <- str_c(str_sub(colnames(mrna)[[i]], 1, 8), "_FDR")
   mrna <- merge(mrna, mrna_fc, by="Gene_Symbol")
 }
 
@@ -333,6 +375,11 @@ for (i in 1:nrow(lncRa1)) {
 lncRa1_num <- cbind(lncRa1, num) %>% 
   as_tibble() 
 
+sum(lncRa1_num$num)/(nrow(lncRa1)*84)
+z <- lncRa1_num %>% filter(num<42) 
+sum(z$num)/(nrow(z)*84)
+lncRa1 %>% ggplot(aes(num))+geom_bar()
+
 lncRna <- lncRa1_num %>% filter(num<42) %>% select(-num)
 
 
@@ -352,10 +399,11 @@ for (i in 2:43) {
   normEXP <- merge(norm, topTags(lrt, n=30000), by.x="row.names", by.y="row.names")
   names(normEXP)[1]
   head(normEXP)
-  lncRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue)
+  lncRna_fc <- data.frame(normEXP$Row.names, normEXP$logFC, normEXP$PValue, normEXP$FDR)
   colnames(lncRna_fc)[1] <- "Gene_Symbol"
   colnames(lncRna_fc)[2] <- str_c(str_sub(colnames(lncRna)[[i]], 1, 8), "_FC")
   colnames(lncRna_fc)[3] <- str_c(str_sub(colnames(lncRna)[[i]], 1, 8), "_pvalue")
+  colnames(lncRna_fc)[4] <- str_c(str_sub(colnames(lncRna)[[i]], 1, 8), "_FDR")
   lncRna <- merge(lncRna, lncRna_fc, by="Gene_Symbol")
 }
 
@@ -366,3 +414,7 @@ rt_fc <- list(miR, piR, mR, lncR, snoR, snR, tR, yR)
 names(rt_fc) <- c("miR", "piR", "mR", "lncR", "snoR", "snR", "tR", "yR")
 save(rt_fc, file="process_data/rt_fc1.Rdata")
 
+
+rt_fc <- list(mirna, pirna, mrna, lncRna, snorna, snRna, tRna, yRna)
+names(rt_fc) <- c("miR", "piR", "mR", "lncR", "snoR", "snR", "tR", "yR")
+save(rt_fc, file="process_data/rt_fc2.Rdata")
